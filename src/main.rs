007c518f9ce12fd550425c2519d12a329fb47c32
Marcel Fox <marcelfox@live.com>
2025-04-10 23:30:05 -0300

    Add timestamp tracking and hourly usage alerts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde::Deserialize;
 use serde::Serialize;
@@ -12,6 +13,7 @@ struct PulseData {
     product_sku: String,
     used_amount: f64,
     use_unit: String,
+    timestamp: Option<String>,
 }
 
 #[tokio::main]
@@ -21,14 +23,35 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
+const ONE_HOUR: i64 = 3600;
+
 async fn func(event: LambdaEvent<PulseData>) -> Result<Value, Error> {
     services::redis::hello();
     let pulse_data = event.payload;
-    let key_name = format!(
+    let usage_key = format!(
         "USAGE:{}:{}:{}",
         pulse_data.tenant, pulse_data.product_sku, pulse_data.use_unit
     );
-    let redis_result = services::redis::set_key_value(key_name, pulse_data.used_amount);
+    let timestamp_key = format!("TIMESTAMP:{}:{}", pulse_data.tenant, pulse_data.product_sku);
+    let found_timestamp_key = services::redis::scan_keys(timestamp_key.clone());
+    if found_timestamp_key.is_empty() {
+        services::redis::set_key_value(timestamp_key, Utc::now().to_rfc3339().to_string());
+        services::redis::set_key_value(usage_key.clone(), pulse_data.used_amount.to_string());
+    } else {
+        let found_timestamp: String = services::redis::get_key_value(found_timestamp_key.clone());
+        let timestamp: DateTime<Utc> = DateTime::parse_from_rfc3339(&found_timestamp)
+            .unwrap()
+            .with_timezone(&Utc);
+        services::redis::increment_key(usage_key.clone(), pulse_data.used_amount.to_string());
+        let elapsed_time = Utc::now().timestamp() - timestamp.timestamp();
+
+        if elapsed_time > ONE_HOUR {
+            println!("Sending Message!");
+            services::redis::delete_key(found_timestamp_key);
+            return Ok(json!({"message": "Passed 1 hour since last update"}));
+        }
+    }
+    let redis_result = services::redis::get_key_value(usage_key);
     let message = format!(
         "Usage amount from tenant '{}' on SKU '{}' has increased to {}",
         pulse_data.tenant, pulse_data.product_sku, redis_result
